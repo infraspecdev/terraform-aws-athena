0520bb8e5e78b1416f406b37fe00b092acc38446
Rahul-4480 <rahul.int24@infraspec.dev>
2024-08-22 12:02:18 +0530

    fix: enhance variable descriptions

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,29 +1,29 @@
 variable "workgroup_name" {
-  description = "(Required) Name of the workgroup"
+  description = "(Required) The name of the Athena workgroup that will manage and isolate query execution and resource usage"
   type        = string
 }
 
 variable "query_results_bucket" {
-  description = "The S3 bucket where Athena query results will be stored."
+  description = "(Required) The name of the S3 bucket where Athena will store the output of executed queries"
   type        = string
 }
 
 variable "database_name" {
-  description = "The name of the Athena database."
+  description = "(Required) The name of the Athena database in the AWS Glue Catalog that will contain the schema definitions for your ALB logs"
   type        = string
 }
 
 variable "s3_bucket_name" {
-  description = "The name of the S3 bucket where ALB logs are stored."
+  description = "(Required) The name of the S3 bucket that holds the raw Application Load Balancer (ALB) logs"
   type        = string
 }
 
 variable "alb_access_logs_table_name" {
-  description = "The table name for the alb access logs"
+  description = "The name of the Glue Catalog table that will store the parsed ALB access logs"
   type        = string
 }
 
 variable "alb_connection_logs_table_name" {
-  description = "The table name for the alb connection logs"
+  description = "The name of the Glue Catalog table that will store the parsed ALB connection logs"
   type        = string
 }
--- examples/minimal/variables.tf
+++ examples/minimal/variables.tf
@@ -1,19 +1,19 @@
 variable "workgroup_name" {
-  description = "(Required) Name of the workgroup"
+  description = "(Required) The name of the Athena workgroup that will manage and isolate query execution and resource usage"
   type        = string
 }
 
 variable "query_results_bucket" {
-  description = "The S3 bucket where Athena query results will be stored."
+  description = "(Required) The name of the S3 bucket where Athena will store the output of executed queries"
   type        = string
 }
 
 variable "database_name" {
-  description = "The name of the Athena database."
+  description = "(Required) The name of the Athena database in the AWS Glue Catalog that will contain the schema definitions for your ALB logs"
   type        = string
 }
 
 variable "s3_bucket_name" {
-  description = "The name of the S3 bucket where ALB logs are stored."
+  description = "(Required) The name of the S3 bucket that holds the raw Application Load Balancer (ALB) logs"
   type        = string
 }
